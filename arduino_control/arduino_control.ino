ac1a0dcf39cdfebf5f9a2d0c1a1e1f667f258cef
dandan002 <danieljung578@gmail.com>
2024-12-08 18:45:20 -0500

    DONE: Servo logic

--- arduino_control/arduino_control.ino
+++ arduino_control/arduino_control.ino
@@ -1,13 +1,16 @@
-// name turrets
-const int panServo = 12
-const int tiltServo = 13
+#include <Servo.h>
+
+// init pan, tilt
+Servo panServo;
+Servo tiltServo;
 
 void setup() {
-  // put your setup code here, to run once:
-
+  // setup servos
+  panServo.attach(12);
+  tiltServo.attach(13);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  panServo.write(50);
 }
