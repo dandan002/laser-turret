c5e3d868a03c95785244a548709e486922c4bc1f
dandan002 <danieljung578@gmail.com>
2024-12-09 02:51:45 -0500

    DONE: Init pan/tilt, pixy code

--- arduino_control/arduino_control.ino
+++ arduino_control/arduino_control.ino
@@ -1,17 +1,29 @@
 #include <Servo.h>
+#include <Pixy2.h>
+#include <SPI.h>
 
 // init pan, tilt
 Servo panServo;
 Servo tiltServo;
+const int panPin = 12;
+const int tiltPin = 13;
 
-// TEST VARS
-int panWrite = 0;
-int tiltWrite = 180;
+// init pan limits
+const int panMin = 0;
+const int panWrite = 90; // 90 = Forward
+const int panMax = 180;
+
+// init tilt limits
+const int tiltMin = 0;
+const int tiltWrite = 90; // 90 = Forward
+const int tiltMax = 180;
+
+Pixy2 pixy;
 
 void setup() {
   // setup servos
-  panServo.attach(12); // 180 = Forward
-  tiltServo.attach(13); // 180 = Forward, 90 = Up, 360 = Down
+  panServo.attach(panPin); // 180 = Forward
+  tiltServo.attach(tiltPin); // 180 = Forward, 90 = Up, 360 = Down
 }
 
 void loop() {
