7cc656dc362f28b6bad42c1412caabb0d7519ada
dandan002 <danieljung578@gmail.com>
2024-12-08 19:37:14 -0500

    Edits to the arduino_control.ino file to add a new function to control the LED brightness.

--- arduino_control/arduino_control.ino
+++ arduino_control/arduino_control.ino
@@ -4,13 +4,18 @@
 Servo panServo;
 Servo tiltServo;
 
+// TEST VARS
+int tiltWrite = 270;
+int panWrite = 180;
+
 void setup() {
   // setup servos
-  panServo.attach(12);
-  tiltServo.attach(13);
+  panServo.attach(12); // 180 = Forward
+  tiltServo.attach(13); // 270 = Forward, 90 = Up, 360 = Down
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  panServo.write(50);
+  panServo.write(panWrite);
+  tiltServo.write(tiltWrite);
 }
