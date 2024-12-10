a82a4bb16bfb087acace9ea8f5d3bdcb1bfd80c1
dandan002 <danieljung578@gmail.com>
2024-12-09 21:59:36 -0500

    DONE: Testing notes

--- arduino_control/arduino_control_v.0.1/arduino_control_v.0.1.ino
+++ arduino_control/arduino_control_v.0.1/arduino_control_v.0.1.ino
@@ -1,69 +1,57 @@
-#include <Pixy2.h>
 #include <Servo.h>
+#include <Pixy2.h>
 
 Pixy2 pixy;
+
 Servo panServo;
 Servo tiltServo;
+Servo steeringServo;
 
 const int panPin = 9;
 const int tiltPin = 10;
+const int motorPin = 12;
+const int steeringPin = 11;
 
-// PID constants
-float Kp = 0.5;
-float Ki = 0.1;
-float Kd = 0.05;
+const int panCenter = 100;
+const int panLeft = 10;
+const int panRight = 200;
 
-// PID variables for pan
-float panSetpoint = 90;
-float panInput, panOutput;
-float panPrevError = 0, panIntegral = 0;
-
-// PID variables for tilt
-float tiltSetpoint = 90;
-float tiltInput, tiltOutput;
-float tiltPrevError = 0, tiltIntegral = 0;
+const int tiltCenter = 180;
+const int tiltUp = 90;
 
 void setup() {
-  Serial.begin(115200);
+  Serial.begin(9600);
   pixy.init();
-  pixy.setLamp(1, 1); // Turn on Pixy2 lights
+  
   panServo.attach(panPin);
   tiltServo.attach(tiltPin);
-  panServo.write(90); // Center position
-  tiltServo.write(90); // Center position
+  steeringServo.attach(steeringPin);
+  
+  pinMode(motorPin, OUTPUT);
+  
+  panServo.write(panCenter);
+  tiltServo.write(tiltCenter);
 }
 
 void loop() {
-  int blocks = pixy.ccc.getBlocks();
+  pixy.ccc.getBlocks();
   
-  if (blocks) {
-    panInput = map(pixy.ccc.blocks[0].m_x, 0, 319, 0, 180);
-    tiltInput = map(pixy.ccc.blocks[0].m_y, 0, 199, 0, 180);
+  if (pixy.ccc.numBlocks) {
+    int x = pixy.ccc.blocks[0].m_x;
+    int y = pixy.ccc.blocks[0].m_y;
     
-    // PID control for pan
-    float panError = panSetpoint - panInput;
-    panIntegral += panError;
-    float panDerivative = panError - panPrevError;
-    panOutput = Kp * panError + Ki * panIntegral + Kd * panDerivative;
-    panPrevError = panError;
+    int panPosition = map(x, 0, 319, panLeft, panRight);
+    int tiltPosition = map(y, 0, 199, tiltUp, tiltCenter);
     
-    // PID control for tilt
-    float tiltError = tiltSetpoint - tiltInput;
-    tiltIntegral += tiltError;
-    float tiltDerivative = tiltError - tiltPrevError;
-    tiltOutput = Kp * tiltError + Ki * tiltIntegral + Kd * tiltDerivative;
-    tiltPrevError = tiltError;
+    panServo.write(panPosition/5);
+    tiltServo.write(tiltPosition/5);
     
-    // Update servo positions
-    panServo.write(constrain(panSetpoint + panOutput, 0, 180));
-    tiltServo.write(constrain(tiltSetpoint + tiltOutput, 0, 180));
+    int steeringPosition = map(x, 0, 319, panLeft, panRight);
+    steeringServo.write(steeringPosition);
     
-    Serial.print("Pan Position: ");
-    Serial.print(panSetpoint + panOutput);
-    Serial.print(" | Tilt Position: ");
-    Serial.println(tiltSetpoint + tiltOutput);
-    
-    delay(100); // wait 100 ms
+    digitalWrite(motorPin, HIGH); // Move forward
+  } else {
+    digitalWrite(motorPin, LOW); // Stop
   }
   
   delay(20);
