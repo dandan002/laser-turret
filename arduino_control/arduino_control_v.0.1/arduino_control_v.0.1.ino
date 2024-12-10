ffb4d60614a59293aba9ddc4109091a2d225711a
dandan002 <danieljung578@gmail.com>
2024-12-10 04:00:00 -0500

    DONE: ALL

--- arduino_control/arduino_control_v.0.1/arduino_control_v.0.1.ino
+++ arduino_control/arduino_control_v.0.1/arduino_control_v.0.1.ino
@@ -24,15 +24,20 @@ const int steerCenter = 90;
 const int steerLeft = 10;
 const int steerRight = 170;
 
-const int centerWidth = pixy.frameWidth/2;
-const int centerHeight = pixy.frameHeight/2;
+const int centerWidth = pixy.frameWidth / 2;
+const int centerHeight = pixy.frameHeight / 2;
 
 int moveSpeed = 1;
+int centerCounter = 0;
+const int centerThreshold = 50; // Number of frames the object needs to be centered
+
+bool turnRight = true;
 
 void setup() {
   Serial.begin(9600);
   pixy.init();
-  
+  pixy.setLamp(1, 1);
+
   panServo.attach(panPin);
   tiltServo.attach(tiltPin);
   steeringServo.attach(steeringPin);
@@ -50,6 +55,7 @@ void loop() {
   if (pixy.ccc.numBlocks) {
     int x = pixy.ccc.blocks[0].m_x;
     int y = pixy.ccc.blocks[0].m_y;
+    int blockWidth = pixy.ccc.blocks[0].m_width;
     
     int frameWidth = pixy.frameWidth;
     int frameHeight = pixy.frameHeight;
@@ -67,21 +73,67 @@ void loop() {
       panServo.write(panServo.read() + moveSpeed);
       steeringServo.write(steeringServo.read() - moveSpeed);
       Serial.println("Moving left");
+      centerCounter = 0;
     } else if (x > 2 * frameWidth / 3) {
       panServo.write(panServo.read() - moveSpeed);
       steeringServo.write(steeringServo.read() + moveSpeed);
       Serial.println("Moving right");
+      centerCounter = 0;
+    } else {
+      centerCounter++;
     }
     
     if (y < frameHeight / 3) {
       tiltServo.write(tiltServo.read() - moveSpeed);
       Serial.println("Moving up");
+      centerCounter = 0;
     } else if (y > 2 * frameHeight / 3) {
       tiltServo.write(tiltServo.read() + moveSpeed);
       Serial.println("Moving down");
+      centerCounter = 0;
+    } else {
+      centerCounter++;
     }
 
-    analogWrite(motorPin, 1500);
+    if (centerCounter >= centerThreshold) {
+      digitalWrite(laserPin, HIGH);
+    } else {
+      digitalWrite(laserPin, LOW);
+    }
+
+    // motor control
+    if (blockWidth < (0.1 * frameWidth)) {
+      analogWrite(motorPin, 60);
+    } else if (blockWidth < (0.2 * frameWidth)) {
+      analogWrite(motorPin, 40);
+    } else {
+      analogWrite(motorPin, 0);
+    }
+  } else {
+    // IF NOT FOUND
+
+    // Turn laser off
+    digitalWrite(laserPin, LOW);
+    centerCounter = 0;
+
+    // Turn off motor
+    analogWrite(motorPin, 0);
+
+    // Look around
+    tiltServo.write(150);
+    if (turnRight) {
+      panServo.write(panServo.read() + moveSpeed);
+      if (panServo.read() >= 150) {
+        turnRight = false;
+      }
+      Serial.println("Look right");
+    } else {
+      panServo.write(panServo.read() - moveSpeed);
+      if (panServo.read() <= 30) {
+        turnRight = true;
+      }
+      Serial.println("Look left");
+    }
   }
   
   delay(20);
